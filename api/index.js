2467a4ab11c75ff15ddc615f1028648bb834a5a6
JCPrime01 <vanzokiller@gmail.com>
2026-05-14 15:22:08 -0300

    update shareCode to 6YQLSZ9F3A3

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=MDISMAA0FUX&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=6YQLSZ9F3A3&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
