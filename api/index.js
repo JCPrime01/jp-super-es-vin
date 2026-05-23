e0d407585adecd2c2fc38f08881db2fff9c9cef9
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-23 11:56:18 -0300

    chore: update shareCode to 8A4NHR5K905

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=I7BJC9EZ3WY&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=8A4NHR5K905&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
