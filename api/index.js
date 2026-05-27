2ad380685b0391d8b301128fe143c96f4bd03961
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-27 10:24:24 -0300

    chore: update shareCode to KBU4KYFX5WN

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=WKMQVWZNFTZ&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=KBU4KYFX5WN&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
