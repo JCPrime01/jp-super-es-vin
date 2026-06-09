ea1fde2b29f8621bd3cb07ec722afea0e667a465
JCPrime01 <vanzokiller@gmail.com>
2026-06-09 10:54:18 -0300

    chore: shareCode TL2YVZSOT3M -> 9G28HRPSEUJ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=TL2YVZSOT3M&afp=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/c7o2gy4l?shareCode=9G28HRPSEUJ&afp=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
