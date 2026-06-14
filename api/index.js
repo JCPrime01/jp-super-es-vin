2af7134cad5646879fdfbba1bfc07b96653c59ea
JCPrime01 <vanzokiller@gmail.com>
2026-06-14 15:37:54 -0300

    chore: shareCode VS4EOYP4U0H -> SE3ZGPC9AP6 (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;                                                                                                                                                                              
     }        
                                                                                                                                                                                            
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=VS4EOYP4U0H&afp1=boost' });                                                                              
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6j30imbu?campaign_id=19735&shareCode=SE3ZGPC9AP6&afp1=boost' });                                                                              
     res.end();                                                                                                                                                                             
   }
